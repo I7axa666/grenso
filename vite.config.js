f1326787fd73876036af6cb04ba37dbb3718c5c9
Paul Solo <p.v.solovey@ya.ru>
2024-10-25 15:33:01 +0300

    vite.config was changed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  base: '/grenso/', // Замените <REPO_NAME> на имя вашего репозитория
-};
+export default defineConfig({
+  base: '/grenso/', // Замените на имя вашего репозитория
+  plugins: [react()],
+});
