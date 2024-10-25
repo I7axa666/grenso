204837e235317297c3876bbc04e377faa13ae5c9
Paul Solo <p.v.solovey@ya.ru>
2024-10-25 15:24:01 +0300

    Add README

---
+++ README.md
@@ -0,0 +1,3 @@
+## Сыллка GH-Pages
+
+https://i7axa666.github.io/grenso/
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  base: '/grenso/', // Замените <REPO_NAME> на имя вашего репозитория
+};
